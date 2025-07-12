18704b8d5e3d6e469fd796fd91a87522449b9869
Luca Conti <conti.1702084@studenti.uniroma1.it>
2025-07-13 01:02:44 +0200

    Tilt changed

--- scr/README.md
+++ scr/README.md
@@ -3,3 +3,4 @@
 1. Nel paper il è CNNblock costituito d LeakyReLU -> BN -> CNN. Ora invece è *CNN -> BN -> SiLU*
 2. Positional encoding 2D
 3. Modifica di (CNNBlock3 + CNNBlock4): ora l'output finale è B x 512 x 3 x 6  
+4. No Masked MHA nel decoder
--- scr/augmentation.py
+++ scr/augmentation.py
@@ -3,14 +3,25 @@ import random
 import cv2
 from PIL import Image, ImageFilter, ImageDraw
 from torchvision import transforms
+import torchvision.transforms.functional as TF
+import random
 
 class RandomColorPad:
-    def __init__(self, pad=(10, 20, 10, 20)):
-        self.pad = pad
+    def __init__(self, pad_y_range=(5, 15), pad_x_range=(10, 30)):
+        self.pad_y_range = pad_y_range  # (min_y, max_y)
+        self.pad_x_range = pad_x_range  # (min_x, max_x)
 
     def __call__(self, img):
+        # Padding casuale per asse Y (top e bottom)
+        vertical_pad = random.randint(*self.pad_y_range)
+
+        # Padding casuale per asse X (left e right)
+        horizontal_pad = random.randint(*self.pad_x_range)
+
+        # Colore casuale
         color = tuple(random.randint(0, 255) for _ in range(3))
-        return transforms.functional.pad(img, padding=self.pad, fill=color)
+
+        return TF.pad(img, (horizontal_pad, vertical_pad, horizontal_pad, vertical_pad), fill=color)
 
 class RandomMotionBlur:
     def __init__(self, p=0.5, kernel_size=(3, 9)):

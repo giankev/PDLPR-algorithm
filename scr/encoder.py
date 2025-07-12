a8861687b57f3c533ff814caaab1a32441e60260
giankev <161844049+giankev@users.noreply.github.com>
2025-07-12 16:04:27 +0200

    Update encoder.py
    change encoder

--- scr/encoder.py
+++ scr/encoder.py
@@ -18,41 +18,47 @@ class PositionalEncoding(nn.Module):
         return x + self.pe[:, :x.size(1), :]
 
 
+class AddAndNorm(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.norm = nn.LayerNorm(in_channels) # NOTE: e se facessimo Group Norm?
+
+    def forward(self, x: torch.Tensor, residual: torch.Tensor) -> torch.Tensor:
+        # (B, in_channels, H , W) -> (B, in_channels, H , W)
+        x = x + residual
+        # (B, in_channels, H , W) -> # (B, H , W, in_channels)
+        x = x.permute(0, 2, 3, 1)
+        # (B, H , W, in_channels) -> (B, H , W, in_channels)
+        x = self.norm(x) 
+        # (B, H , W, in_channels) -> (B, in_channels, H , W)
+        x = x.permute(0, 3, 1, 2)
+        return x
+
 class EncoderUnit(nn.Module):
     def __init__(self, in_channels: int, d_embed: int, n_heads: int):
         super().__init__()
-        # NOTE: [!] se metto padding = 1 le dimensioni di x aumentano di 2 sia di height che di width
+        # NOTE: [!] se metto padding = 1 (come nel paper) le dimensioni di x aumentano di 2 sia di height che di width 
         self.cnn1 = CNNBlock(in_channels=in_channels, out_channels=d_embed, kernel_size=1, stride=1, padding=0)
         self.attention = SelfAttention(n_heads=n_heads, d_embed=d_embed)
-        # NOTE: [!] se metto padding = 1 le dimensioni di x aumentano
+        # NOTE: [!] se metto padding = 1 ((come nel paper)) le dimensioni di x aumentano di 2 sia di height che di width
         self.cnn2 = CNNBlock(in_channels=d_embed, out_channels=in_channels, kernel_size=1, stride=1, padding=0) 
-        self.norm = nn.GroupNorm(num_groups=1, num_channels=in_channels)
+        self.addnorm = AddAndNorm(in_channels)  
 
     def forward(self, x):
         # x: (B, in_channels, H, W)
         residual = x
         # (B, in_channels, H, W) -> (B, d_embed, H, W)
         x = self.cnn1(x)
-        b, c, h, w = x.shape
-
-        # (B, d_embed, H, W) -> (B, d_embed, H * W)
-        x = x.view((b, c, h * w))
-        # (B, d_embed, H * W) -> (B, H * W, d_embed)
-        x = x.transpose(-1, -2)
         # Self-attention WITHOUT mask
-        # (B, H * W, d_embed) -> (B, H * W, d_embed)
+        # (B, d_embed, H, W) -> (B, d_embed, H , W) 
         x = self.attention(x)
-        # (B, H * W, d_embed) ->  (B, C, H * d_embed) 
-        x = x.transpose(-1, -2)
-        # (B, d_embed, H * W)  -> (B, d_embed, H , W) 
-        x = x.view((b, c, h, w))
         #  (B, d_embed, H , W) -> (B, in_channels, H , W) 
         x = self.cnn2(x)
-        # (B, in_channels, H , W) -> (B, in_channels, H , W)
-        x = x + residual
-        x = self.norm(x) 
+        #(B, in_channels, H , W) -> (B, in_channels, H , W)
+        x = self.addnorm(x, residual)
 
-        return x
+        return x # (B, in_channels, H , W)
+
 
 class Encoder(nn.Module):
     def __init__(self, in_channels=512,
@@ -74,18 +80,3 @@ class Encoder(nn.Module):
             EncoderUnit(in_channels, out_channels, n_heads)
             for _ in range(enc_unit)
         ])
-
-    def forward(self, x):
-        # x: (B, in_channels, H, W)
-        B, C, H, W = x.shape
-
-        # (B, C, H, W) -> (B, C, H*W) -> (B, H*W, C)
-        x = x.flatten(2).permute(0, 2, 1)
-        # (B, H*W, C)
-        x = self.pos_encoder(x)
-        # (B, H*W, C) -> (B, C, H*W) -> (B, C, H, W)
-        x = x.permute(0, 2, 1).reshape(B, C, H, W)
-
-        for layer in self.layers:
-            x = layer(x)
-        return x
